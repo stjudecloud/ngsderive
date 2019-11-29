ff1552ec97e1548c47b36b2ba1b8d76d98fb4b36
Clay McLeod <Clay.McLeod@STJUDE.org>
2019-11-29 13:44:47 -0600

    improvement(instrument): improve subcommand and switch to argparse

--- README.md
+++ README.md
@@ -26,22 +26,24 @@ pip install git+https://github.com/claymcleod/ngsderive.git
 
 ### Illumina machine type
 
-The `ngsderive instrument` subcommand will attempt to backward compute the
+The `instrument` subcommand will attempt to backward compute the
 machine that generated a NGS file using (1) the instrument id(s) and (2) the
-flowcell id(s). Note that this command may not comprehensively detect the
-correct machines as there is no published catalog of Illumina serial numbers.
-As we encounter more serial numbers in practice, we update this code.
+flowcell id(s). 
+
+### Limitations
+
+* This command may not comprehensively detect the correct machines as there is no published catalog of Illumina serial numbers. As we encounter more serial numbers in practice, we update this code.
 
 ### Read length calculation
 
-Using the `readlen` subcommand, the readlength used during sequencing can be
+The `readlen` subcommand can be used to compute the read length used during sequencing can be
 estimated (or reported as inconclusive). At the time of writing, the algorithm used 
 is roughly:
 
 1. Compute distribution of read lengths for the first `--n-samples` reads in a file.
-2. Assuming readlength in the file can only decrease from the actual read length (from adapter trimming or similar), the putative maximum readlength is considered to be the highest detected read length.
-3. If the percentage of reads that are evidence for the putative maximum readlength makes up at least `--majority-vote-cutoff`% of the reads, the putative read length is considered to be confirmed. If not, the consensus read length will be return as -1 (could not determine).
-   1. For example, if 100bp is the maxmimum read length detected and 85% percent of the reads support that claim, then we considered 100bp as the consensus readlength. If only 30% of the reads indicated 100bp, the tool cannot report a consensus).
+2. Assuming read length in the file can only decrease from the actual read length (from adapter trimming or similar), the putative maximum read length is considered to be the highest detected read length.
+3. If the percentage of reads that are evidence for the putative maximum read length makes up at least `--majority-vote-cutoff`% of the reads, the putative read length is considered to be confirmed. If not, the consensus read length will be return as -1 (could not determine).
+   1. For example, if 100bp is the maximum read length detected and 85% percent of the reads support that claim, then we considered 100bp as the consensus read length. If only 30% of the reads indicated 100bp, the tool cannot report a consensus).
 
 ### Strandedness inference
 
--- ngsderive/commands/instrument.py
+++ ngsderive/commands/instrument.py
@@ -2,6 +2,7 @@ import csv
 import itertools
 import pysam
 import re
+import sys
 
 import logging
 from collections import defaultdict
@@ -10,159 +11,183 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 instrument_ids = {
-    "^HWI-M[0-9]{4}$" : ["MiSeq"],
-    "^HWUSI" : ["Genome Analyzer IIx"],
-    "^M[0-9]{5}$" : ["MiSeq"],
-    "^HWI-C[0-9]{5}$" : ["HiSeq 1500"],
-    "^C[0-9]{5}$" : ["HiSeq 1500"],
-    "^HWI-ST[0-9]{3,5}(_[0-9]{9})?$" : ["HiSeq 2000"],
-    "^HWI-D[0-9]{5}$" : ["HiSeq 2000", "HiSeq 2500"],
-    "^A[0-9]{5}$" : ["NovaSeq"],
-    "^D[0-9]{5}$" : ["HiSeq 2500"],
-    "^J[0-9]{5}$" : ["HiSeq 3000"],
-    "^K[0-9]{5}$" : ["HiSeq 3000","HiSeq 4000"],
-    "^E[0-9]{5}$" : ["HiSeq X"],
-    "^N[0-9]{5}$" : ["NextSeq"],
+    "^HWI-M[0-9]{4}$": ["MiSeq"],
+    "^HWUSI": ["Genome Analyzer IIx"],
+    "^M[0-9]{5}$": ["MiSeq"],
+    "^HWI-C[0-9]{5}$": ["HiSeq 1500"],
+    "^C[0-9]{5}$": ["HiSeq 1500"],
+    "^HWI-ST[0-9]{3,5}(_[0-9]{9})?$": ["HiSeq 2000"],
+    "^HWI-D[0-9]{5}$": ["HiSeq 2000", "HiSeq 2500"],
+    "^A[0-9]{5}$": ["NovaSeq"],
+    "^D[0-9]{5}$": ["HiSeq 2500"],
+    "^J[0-9]{5}$": ["HiSeq 3000"],
+    "^K[0-9]{5}$": ["HiSeq 3000", "HiSeq 4000"],
+    "^E[0-9]{5}$": ["HiSeq X"],
+    "^N[0-9]{5}$": ["NextSeq"],
     "^NB[0-9]{6}$": ["NextSeq"],
-    "^NS[0-9]{6}$" : ["NextSeq"],
-    "^MN[0-9]{5}$" : ["MiniSeq"],
+    "^NS[0-9]{6}$": ["NextSeq"],
+    "^MN[0-9]{5}$": ["MiniSeq"],
 }
 
 flowcell_ids = {
-    "^C[A-Z0-9]{4}ANXX$" : ["HiSeq 1500", "HiSeq 2000", "HiSeq 2500"],               # High Output (8-lane) v4 flow cell
-    "^C[A-Z0-9]{4}ACXX$" : ["HiSeq 1000", "HiSeq 1500", "HiSeq 2000", "HiSeq 2500"], # High Output (8-lane) v3 flow cell
-    "^D[A-Z0-9]{4}ACXX$" : ["HiSeq 1000", "HiSeq 1500", "HiSeq 2000", "HiSeq 2500"], # High Output (8-lane) v3 flow cell
-    "^H[A-Z0-9]{4}ADXX$" : ["HiSeq 1500", "HiSeq 2000", "HiSeq 2500"],               # Rapid Run (2-lane) v1 flow cell
-    "^H[A-Z0-9]{4}BCXX$" : ["HiSeq 1500", "HiSeq 2500"],                             # Rapid Run (2-lane) v2 flow cell
-    "^H[A-Z0-9]{4}BCXY$" : ["HiSeq 1500", "HiSeq 2500"],                             # Rapid Run (2-lane) v2 flow cell
-    "^H[A-Z0-9]{4}BBXX$" : ["HiSeq 4000"],                                           # (8-lane) v1 flow cell
-    "^H[A-Z0-9]{4}BBXY$" : ["HiSeq 4000"],                                           # (8-lane) v1 flow cell
-    "^H[A-Z0-9]{4}CCXX$" : ["HiSeq X"],                                              # (8-lane) flow cell
-    "^H[A-Z0-9]{4}CCXY$" : ["HiSeq X"],                                              # (8-lane) flow cell
-    "^H[A-Z0-9]{4}ALXX$" : ["HiSeq X"],                                              # (8-lane) flow cell
-    "^H[A-Z0-9]{4}BGX[A-Z,0-9]$" : ["NextSeq"],                                      # High output flow cell
-    "^H[A-Z0-9]{4}AFXX$" : ["NextSeq"],                                              # Mid output flow cell
-    "^H[A-Z0-9]{5}RXX$" : ["NovaSeq"],                                               # S1 flow cell
-    "^H[A-Z0-9]{5}RXX$" : ["NovaSeq"],                                               # SP flow cell
-    "^H[A-Z0-9]{5}MXX$" : ["NovaSeq"],                                               # S2 flow cell
-    "^H[A-Z0-9]{5}SXX$" : ["NovaSeq"],                                               # S4 flow cell
-    "^A[A-Z0-9]{4}$" : ["MiSeq"],                                                    # MiSeq flow cell
-    "^B[A-Z0-9]{4}$" : ["MiSeq"],                                                    # MiSeq flow cell
-    "^D[A-Z0-9]{4}$" : ["MiSeq"],                                                    # MiSeq nano flow cell
+    "^C[A-Z0-9]{4}ANXX$": ["HiSeq 1500", "HiSeq 2000",
+                           "HiSeq 2500"],  # High Output (8-lane) v4 flow cell
+    "^C[A-Z0-9]{4}ACXX$":
+    ["HiSeq 1000", "HiSeq 1500", "HiSeq 2000",
+     "HiSeq 2500"],  # High Output (8-lane) v3 flow cell
+    "^D[A-Z0-9]{4}ACXX$":
+    ["HiSeq 1000", "HiSeq 1500", "HiSeq 2000",
+     "HiSeq 2500"],  # High Output (8-lane) v3 flow cell
+    "^H[A-Z0-9]{4}ADXX$": ["HiSeq 1500", "HiSeq 2000",
+                           "HiSeq 2500"],  # Rapid Run (2-lane) v1 flow cell
+    "^H[A-Z0-9]{4}BCXX$": ["HiSeq 1500",
+                           "HiSeq 2500"],  # Rapid Run (2-lane) v2 flow cell
+    "^H[A-Z0-9]{4}BCXY$": ["HiSeq 1500",
+                           "HiSeq 2500"],  # Rapid Run (2-lane) v2 flow cell
+    "^H[A-Z0-9]{4}BBXX$": ["HiSeq 4000"],  # (8-lane) v1 flow cell
+    "^H[A-Z0-9]{4}BBXY$": ["HiSeq 4000"],  # (8-lane) v1 flow cell
+    "^H[A-Z0-9]{4}CCXX$": ["HiSeq X"],  # (8-lane) flow cell
+    "^H[A-Z0-9]{4}CCXY$": ["HiSeq X"],  # (8-lane) flow cell
+    "^H[A-Z0-9]{4}ALXX$": ["HiSeq X"],  # (8-lane) flow cell
+    "^H[A-Z0-9]{4}BGX[A-Z,0-9]$": ["NextSeq"],  # High output flow cell
+    "^H[A-Z0-9]{4}AFXX$": ["NextSeq"],  # Mid output flow cell
+    "^H[A-Z0-9]{5}RXX$": ["NovaSeq"],  # S1 flow cell
+    "^H[A-Z0-9]{5}RXX$": ["NovaSeq"],  # SP flow cell
+    "^H[A-Z0-9]{5}MXX$": ["NovaSeq"],  # S2 flow cell
+    "^H[A-Z0-9]{5}SXX$": ["NovaSeq"],  # S4 flow cell
+    "^A[A-Z0-9]{4}$": ["MiSeq"],  # MiSeq flow cell
+    "^B[A-Z0-9]{4}$": ["MiSeq"],  # MiSeq flow cell
+    "^D[A-Z0-9]{4}$": ["MiSeq"],  # MiSeq nano flow cell
     # "^D[A-Z0-9]{4}$" : ["HiSeq 2000", "HiSeq 2500"],                                 # Unknown HiSeq flow cell examined in SJ data
-    "^G[A-Z0-9]{4}$" : ["MiSeq"],                                                    # MiSeq micro flow cell
+    "^G[A-Z0-9]{4}$": ["MiSeq"],  # MiSeq micro flow cell
 }
 
-upgrade_sets = [
-  (set(["HiSeq 2000", "HiSeq 2500"]), ["HiSeq 2000", "HiSeq 2500"])
-]
+upgrade_sets = [(set(["HiSeq 2000",
+                      "HiSeq 2500"]), ["HiSeq 2000", "HiSeq 2500"])]
+
 
 def derive_instrument_from_iid(iid):
-  matching_instruments = set()
+    matching_instruments = set()
 
-  for pattern in instrument_ids.keys():
-    if re.search(pattern, iid):
-      matching_instruments |= set(instrument_ids[pattern])
+    for pattern in instrument_ids.keys():
+        if re.search(pattern, iid):
+            matching_instruments |= set(instrument_ids[pattern])
 
-  return matching_instruments
+    return matching_instruments
 
 
 def predict_instrument_from_iids(iids):
-  possible_instruments_by_iid = set()
-  detected_at_least_one_instrument = False
+    possible_instruments_by_iid = set()
+    detected_at_least_one_instrument = False
 
-  for iid in iids:
-    if len(possible_instruments_by_iid) == 0:
-      possible_instruments_by_iid = derive_instrument_from_iid(iid)
-      detected_at_least_one_instrument = True
-    else:
-      possible_instruments_by_iid &= derive_instrument_from_iid(iid)
+    for iid in iids:
+        if len(possible_instruments_by_iid) == 0:
+            possible_instruments_by_iid = derive_instrument_from_iid(iid)
+            detected_at_least_one_instrument = True
+        else:
+            possible_instruments_by_iid &= derive_instrument_from_iid(iid)
 
-  return possible_instruments_by_iid, detected_at_least_one_instrument
+    return possible_instruments_by_iid, detected_at_least_one_instrument
 
 
 def derive_instrument_from_fcid(fcid):
-  matching_instruments = set()
-  detected_at_least_one_instrument = False
+    matching_instruments = set()
+    detected_at_least_one_instrument = False
 
-  for pattern in flowcell_ids.keys():
-    if re.search(pattern, fcid):
-      matching_instruments |= set(flowcell_ids[pattern])
+    for pattern in flowcell_ids.keys():
+        if re.search(pattern, fcid):
+            matching_instruments |= set(flowcell_ids[pattern])
 
-  return matching_instruments
+    return matching_instruments
 
 
 def predict_instrument_from_fcids(fcids):
-  possible_instruments_by_fcid = set()
-  detected_at_least_one_instrument = False
+    possible_instruments_by_fcid = set()
+    detected_at_least_one_instrument = False
 
-  for fcid in fcids:
+    for fcid in fcids:
+        if len(possible_instruments_by_fcid) == 0:
+            possible_instruments_by_fcid = derive_instrument_from_fcid(fcid)
+            detected_at_least_one_instrument = True
+        else:
+            possible_instruments_by_fcid &= derive_instrument_from_fcid(fcid)
+
+    return possible_instruments_by_fcid, detected_at_least_one_instrument
+
+
+def resolve_instrument(possible_instruments_by_iid,
+                       possible_instruments_by_fcid,
+                       at_least_one_instrument_detected):
+    if len(possible_instruments_by_iid) == 0 and len(
+            possible_instruments_by_fcid) == 0:
+        if at_least_one_instrument_detected:
+            return set(
+                ["multiple instruments"]
+            ), "unknown confidence", "multiple instruments were detected in this sample"
+        else:
+            return set(["unknown"]), "no confidence", "no match"
+
+    if len(possible_instruments_by_iid) == 0:
+        confidence = "medium confidence"
+        if len(possible_instruments_by_fcid) > 1:
+            confidence = "low confidence"
+        return possible_instruments_by_fcid, confidence, "flowcell id"
+
     if len(possible_instruments_by_fcid) == 0:
-      possible_instruments_by_fcid = derive_instrument_from_fcid(fcid)
-      detected_at_least_one_instrument = True
+        confidence = "medium confidence"
+        if len(possible_instruments_by_fcid) > 1:
+            confidence = "low confidence"
+        return possible_instruments_by_iid, confidence, "instrument id"
+
+    overlapping_instruments = possible_instruments_by_iid & possible_instruments_by_fcid
+    if len(overlapping_instruments) == 0:
+        return set(
+            ["conflicting evidence"]
+        ), "high confidence", "Case needs triaging: {} by iid, {} by fcid".format(
+            " or ".join(possible_instruments_by_iid),
+            " or ".join(possible_instruments_by_fcid))
     else:
-      possible_instruments_by_fcid &= derive_instrument_from_fcid(fcid)
-
-  return possible_instruments_by_fcid, detected_at_least_one_instrument
+        return overlapping_instruments, "high confidence", "instrument id and flowcell id"
 
 
-def resolve_instrument(possible_instruments_by_iid, possible_instruments_by_fcid, at_least_one_instrument_detected):
-  if len(possible_instruments_by_iid) == 0 and len(possible_instruments_by_fcid) == 0:
-    if at_least_one_instrument_detected:
-      return set(["multiple instruments"]), "unknown confidence", "multiple instruments were detected in this sample"
-    else:
-      return set(["unknown"]), "no confidence", "no match"
+def main(ngsfiles, outfile=sys.stdout, delimiter="\t", n_samples=10000):
+    writer = csv.DictWriter(
+        outfile,
+        fieldnames=["File", "Instrument", "Confidence", "Basis"],
+        delimiter=delimiter)
+    writer.writeheader()
 
-  if len(possible_instruments_by_iid) == 0:
-    confidence = "medium confidence"
-    if len(possible_instruments_by_fcid) > 1:
-      confidence = "low confidence"
-    return possible_instruments_by_fcid, confidence, "flowcell id"
+    for ngsfile in ngsfiles:
+        samfile = pysam.AlignmentFile(ngsfile, "rb")
 
-  if len(possible_instruments_by_fcid) == 0:
-    confidence = "medium confidence"
-    if len(possible_instruments_by_fcid) > 1:
-      confidence = "low confidence"
-    return possible_instruments_by_iid, confidence, "instrument id"
-  
-  overlapping_instruments = possible_instruments_by_iid & possible_instruments_by_fcid
-  if len(overlapping_instruments) == 0:
-    return set(["conflicting evidence"]), "high confidence", "Case needs triaging: {} by iid, {} by fcid".format(" or ".join(possible_instruments_by_iid), " or ".join(possible_instruments_by_fcid))
-  else: 
-    return overlapping_instruments, "high confidence", "instrument id and flowcell id"
+        instruments = set()
+        flowcells = set()
 
+        # accumulate read lengths
+        for read in itertools.islice(samfile, n_samples):
+            parts = read.query_name.split(":")
+            iid, fcid = parts[0], parts[2]
+            instruments.add(iid)
+            flowcells.add(fcid)
 
-def main(ngsfiles, outfile, n_samples=10):
-  writer = csv.DictWriter(outfile, fieldnames=["File", "Instrument", "Confidence", "Basis"], delimiter="\t")
-  writer.writeheader()
+        possible_instruments_by_iid, detected_instrument_by_iid = predict_instrument_from_iids(
+            instruments)
+        possible_instruments_by_fcid, detected_instrument_by_fcid = predict_instrument_from_fcids(
+            flowcells)
 
-  for ngsfile in ngsfiles:
-    samfile = pysam.AlignmentFile(ngsfile, "rb")
+        instruments, confidence, based_on = resolve_instrument(
+            possible_instruments_by_iid, possible_instruments_by_fcid,
+            detected_instrument_by_iid | detected_instrument_by_fcid)
+        for upgrade_set in upgrade_sets:
+            if instruments.issubset(upgrade_set[0]):
+                instruments = upgrade_set[1]
+                break
 
-    instruments = set()
-    flowcells = set()
+        result = {
+            "File": ngsfile,
+            "Instrument": " or ".join(instruments),
+            "Confidence": confidence,
+            "Basis": based_on
+        }
 
-    # accumulate read lengths
-    for read in itertools.islice(samfile, n_samples):
-      parts = read.query_name.split(":")
-      iid, fcid = parts[0], parts[2]
-      instruments.add(iid)
-      flowcells.add(fcid)
-
-    possible_instruments_by_iid, detected_instrument_by_iid = predict_instrument_from_iids(instruments)
-    possible_instruments_by_fcid, detected_instrument_by_fcid = predict_instrument_from_fcids(flowcells)
-    
-    instruments, confidence, based_on = resolve_instrument(possible_instruments_by_iid, possible_instruments_by_fcid, detected_instrument_by_iid | detected_instrument_by_fcid)
-    for upgrade_set in upgrade_sets:
-      if instruments.issubset(upgrade_set[0]):
-        instruments = upgrade_set[1]
-        break
-
-    result = {
-      "File": ngsfile,
-      "Instrument": " or ".join(instruments),
-      "Confidence": confidence,
-      "Basis": based_on
-    }
-
-    writer.writerow(result)
+        writer.writerow(result)
