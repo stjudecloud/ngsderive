cacf6462e1a9122e8723ae9ad9d9834c4ac9384a
Andrew Frantz <andrew.frantz@stjude.org>
2020-12-29 11:14:11 -0500

    fix: remove faulty periodic check

--- ngsderive/commands/encoding.py
+++ ngsderive/commands/encoding.py
@@ -11,7 +11,6 @@ from ..utils import NGSFile
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-CHECK_INTERVAL = 10000
 SANGER_SET = set([i for i in range(33, 127)])
 ILLUMINA_1_0_SET = set([i for i in range(59, 127)])
 ILLUMINA_1_3_SET = set([i for i in range(64, 127)])
@@ -33,28 +32,20 @@ def main(ngsfiles,
         read_lengths = defaultdict(int)
         try:
             ngsfile = NGSFile(ngsfilepath, store_qualities=True)
-        except:
+        except FileNotFoundError:
             result = {
                 "File": ngsfilepath,
                 "Evidence": "File not found.",
                 "ProbableEncoding": "N/A"
             }
-
             writer.writerow(result)
             outfile.flush()
             continue
 
         score_set = set()
-        # accumulate read lengths
-        total_reads_sampled = 0
         for read in itertools.islice(ngsfile, n_samples):
-            total_reads_sampled += 1
             for char in read['quality']:
                 score_set.add(ord(char))
-            if total_reads_sampled % CHECK_INTERVAL == 0 and not score_set.issubset(ILLUMINA_1_0_SET):
-                # Phred scores must either be in the Sanger or an unknown encoding
-                break
-
         
         max_phred_score = chr(max(score_set))
         min_phred_score = chr(min(score_set))
